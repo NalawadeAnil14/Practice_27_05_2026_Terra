aa769afc72f24ca4ad8159e467d0e5df22f164a0
Anil Nalawade <nalawadeanil14@gmail>
2026-05-27 18:27:50 +0000

    Check 1 server for eac az task

--- test.tf
+++ test.tf
@@ -19,8 +19,13 @@ data "aws_availability_zones" "available" {
 
 resource "aws_instance" "myec2" {
 
-  for_each      = toset(data.aws_availability_zones.available.names)
-  ami           = "ami-00e801948462f718a"
-  instance_type = "t3.micro"
+  for_each          = toset(slice(data.aws_availability_zones.available.names,0,3))
+  ami               = "ami-00e801948462f718a"
+  instance_type     = "t3.micro"
+  availability_zone = each.value
 
+}
+
+output "instance_ids" {
+  value = { for az, instance in aws_instance.myec2 : az => instance.id }
 }
