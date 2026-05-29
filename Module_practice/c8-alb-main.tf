4bbee30d8b2f03a0cd443f310f180bf152a875c4
Anil Nalawade <nalawadeanil14@gmail>
2026-05-29 15:16:58 +0000

    small change

--- Module_practice/c8-alb-main.tf
+++ Module_practice/c8-alb-main.tf
@@ -7,10 +7,10 @@ module "alb" {
   name               = "pract-alb"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [module.alb-sg.this_security_group_id]
+  security_groups    = [module.alb-sg.security_group_id]
 
   vpc_id  = module.my_vpc.vpc_id
-  subnets = module.my_vpc.public_subnets
+  subnets = values(module.my_vpc.public_subnet_ids)
 
   enable_deletion_protection = false
 
@@ -50,7 +50,7 @@ module "alb" {
 }
 
 resource "aws_lb_target_group_attachment" "my_http_target_group_attachment" {
-  for_each         = toset(module.privatevm.private_instance_ids)
+  for_each         = toset(values(module.ec2-server)[*].instance_id)
   target_group_arn = module.alb.target_groups["my-http-target-group"].arn
   target_id        = each.value
   port             = 80
